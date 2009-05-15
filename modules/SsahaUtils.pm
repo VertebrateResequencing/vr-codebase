9fbbbc7a57212fa91c398be7873e68b42284b31c
ar12 <ar12@sanger.ac.uk>
2009-05-15 09:37:54 +0000

    Initial commit of a newly spun out method to filter mouse 454 ssaha hits.

---
+++ modules/SsahaUtils.pm
@@ -0,0 +1,109 @@
+package SsahaUtils;
+
+use strict;
+
+# defines
+my $SSAHA_SCORE_THRESHOLD = 20;
+my $SSAHA_FOLD_DIFFERENCE = 1.5;
+
+my $DEBUG = 0;
+
+
+sub parseForMouseInsertionSites {
+
+  my $ssaha_hits_hash_ref = shift;
+
+  my %results_hash;
+
+  foreach my $curr_seq ( keys %$ssaha_hits_hash_ref ) {
+
+    $results_hash{$curr_seq}{status} = 'NA';
+    $results_hash{$curr_seq}{chr} = 'NA';
+    $results_hash{$curr_seq}{genome_start} = 'NA';
+    $results_hash{$curr_seq}{genome_end} = 'NA';
+    $results_hash{$curr_seq}{alignment_score} = 'NA';
+    $results_hash{$curr_seq}{mapping_score} = 'NA';
+    $results_hash{$curr_seq}{strand} = 'NA';
+
+    # if ssaha returns no alignments for a trace
+    if ( !defined $$ssaha_hits_hash_ref{$curr_seq} ) {
+      $results_hash{$curr_seq}{status} = 'no_genome_alignment';
+      next;
+    }
+
+    # if the best alignment is above the score threshold
+    if ($$ssaha_hits_hash_ref{$curr_seq}[0]->{score} >= $SSAHA_SCORE_THRESHOLD){
+
+      my $best_hit = $$ssaha_hits_hash_ref{$curr_seq}[0];
+      my $chr = '';
+      my $mapping_score = -1;
+
+      # very clunky ...
+
+      # check for multiple alignments
+      if(defined $$ssaha_hits_hash_ref{$curr_seq}[1]) {
+
+	#print "Number of alignments => " . scalar @{$$ssaha_hits_hash_ref{$curr_seq}} . "\n";
+	my $num_of_alignments = scalar @{$$ssaha_hits_hash_ref{$curr_seq}};
+
+	#print $$ssaha_hits_hash_ref{$curr_seq}[0]->{score}  . 
+	#" " .  $$ssaha_hits_hash_ref{$curr_seq}[1]->{score} . " ";
+	#print $num_of_alignments . "\n";
+
+	$mapping_score = 
+	  ( $$ssaha_hits_hash_ref{$curr_seq}[0]->{score} - $$ssaha_hits_hash_ref{$curr_seq}[1]->{score} )
+	    - ( 2 * scalar @{$$ssaha_hits_hash_ref{$curr_seq}} );
+
+	# if the second best score is less than the threshold score
+	# flag the best score as being unique
+	if ( $$ssaha_hits_hash_ref{$curr_seq}[1]->{score} < $SSAHA_SCORE_THRESHOLD ) {
+	  $chr = $best_hit->{'s_name'};
+	  $results_hash{$curr_seq}{status} = 'unique_alignment';
+	}
+
+	# if the difference between the best and second best hit is
+	# above some threshold, again flag the hit as being unique
+	elsif ( $$ssaha_hits_hash_ref{$curr_seq}[0]->{score} >=
+		$SSAHA_FOLD_DIFFERENCE * $$ssaha_hits_hash_ref{$curr_seq}[1]->{score}) {
+
+	  $chr = $best_hit->{'s_name'};
+	  $results_hash{$curr_seq}{status} = 'unique_alignment';
+	}
+
+	# if the second best hit, is 'close' then flag the trace as
+	# having multiple alignments
+	else {
+	  $chr = "MULTI";
+	  $results_hash{$curr_seq}{status} = 'multiple_alignments';
+	}
+      }
+      else {
+	$chr = $best_hit->{'s_name'};
+	$results_hash{$curr_seq}{status} = 'unique_alignment';
+	$mapping_score = 255;
+      }
+
+      # store the alignment result into the results hash
+      $results_hash{$curr_seq}{chr} = $chr;
+      $results_hash{$curr_seq}{genome_start} = $best_hit->{'s_start'};
+      $results_hash{$curr_seq}{genome_end} = $best_hit->{'s_end'};
+      $results_hash{$curr_seq}{alignment_score} = $best_hit->{'score'};
+      $results_hash{$curr_seq}{mapping_score} = $mapping_score;
+
+      # sort out the strand
+      if ( $best_hit->{'direction'} eq 'F' ) {
+	$results_hash{$curr_seq}{strand} = '+';
+      }
+      elsif ( $best_hit->{'direction'} eq 'C' ) {
+	$results_hash{$curr_seq}{strand} = '-';
+      }
+    }
+    else {
+      $results_hash{$curr_seq}{status} = 'weak_alignment';
+    }
+  }
+
+  return (\%results_hash);
+}
+
+1;
