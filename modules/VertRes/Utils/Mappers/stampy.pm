08c6dd6e48762ecc4290dbc2c327f08b1fcca823
sb10 <sb10@sanger.ac.uk>
2010-11-05 11:04:47 +0000

    looks like 10GB is enough

--- modules/VertRes/Utils/Mappers/stampy.pm
+++ modules/VertRes/Utils/Mappers/stampy.pm
@@ -60,7 +60,7 @@ sub _bsub_opts {
     my %bsub_opts = (bsub_opts => '');
     
     if ($action eq 'map') {
-        $bsub_opts{bsub_opts} = '-q long -M12000000 -R \'select[mem>12000] rusage[mem=12000]\'';
+        $bsub_opts{bsub_opts} = '-q long -M10000000 -R \'select[mem>10000] rusage[mem=10000]\'';
     }
     else {
         return $self->SUPER::_bsub_opts($lane_path, $action);
