df81f1332e1feed51c5d790fc9107ef32dcc35da
sb10 <sb10@sanger.ac.uk>
2010-12-15 11:16:33 +0000

    updated to update scripts dir as well; now work faster by assuming users node has write access to /software

--- scripts/update_softwarevertres.pl
+++ scripts/update_softwarevertres.pl
@@ -3,12 +3,13 @@
 use strict;
 use warnings;
 
-open(my $pipe, "| ssh etch-dev64") || die "failed to open pipe\n";
+#open(my $pipe, "| ssh etch-dev64") || die "failed to open pipe\n";
 
-print $pipe "svn update /software/vertres/bin\n";
-print $pipe "svn update /software/vertres/modules\n";
-print $pipe "exit\n";
+system("svn update /software/vertres/bin");
+system("svn update /software/vertres/scripts");
+system("svn update /software/vertres/modules");
+#print $pipe "exit\n";
 
-close($pipe) || die "failed to close pipe\n";
+#close($pipe) || die "failed to close pipe\n";
 
 exit;
