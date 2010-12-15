d4956fe32f78db0cd246649a3bf389b4880b3c18
sb10 <sb10@sanger.ac.uk>
2010-12-15 16:03:54 +0000

    made setting up the VRTrack db optional; still need to have the VRTrack tests skip gracefully in that case

--- build_lib/MyBuild.pm
+++ build_lib/MyBuild.pm
@@ -11,20 +11,24 @@ use VRTrack::VRTrack;
 
 sub create_vrconfig_file {
     my $self     = shift;
-    my $config   =
-    {   
-        host     => $self->prompt( 'VRTrack test DB host: ', 'mcs4a' ),
-        port     => $self->prompt( 'VRTrack test DB port: ', '3306' ),
-        user     => $self->prompt( 'VRTrack test DB user: ', 'vreseq_rw' ),
-        password => $self->prompt( 'VRTrack test DB password: ', 't3aml3ss' ),
-        test_db  => $self->prompt( 'VRTrack test DB name: ', 'vrtrack_test' ),
-    };
-    $self->notes( db_config    => $config );
-    mkpath( catdir( qw( modules VRTrack  ) ) );
-    my $dd       = Data::Dumper->new( [ $config ], [ 'db_config' ] );
-    my $path     = catfile(qw( modules VRTrack Testconfig.pm ));
-    open( my $file, '>', $path ) or die "Cannot write to '$path': $!\n";
-    printf $file <<'END_HERE', $dd->Dump();
+    
+    my $configure_vrdb = $self->prompt('Do you wish to setup a test db for VRTrack? [y/n]: ', 'y');
+    
+    if ($configure_vrdb eq 'y') {
+        my $config   =
+        {   
+            host     => $self->prompt( 'VRTrack test DB host: ', $ENV{VRTRACK_HOST} || 'localhost' ),
+            port     => $self->prompt( 'VRTrack test DB port: ', $ENV{VRTRACK_PORT} || 3306 ),
+            user     => $self->prompt( 'VRTrack test DB user: ', $ENV{VRTRACK_RW_USER} || '' ),
+            password => $self->prompt( 'VRTrack test DB password: ', $ENV{VRTRACK_PASSWORD} || '' ),
+            test_db  => $self->prompt( 'VRTrack test DB name: ', 'vrtrack_test' ),
+        };
+        $self->notes( db_config    => $config );
+        mkpath( catdir( qw( modules VRTrack  ) ) );
+        my $dd       = Data::Dumper->new( [ $config ], [ 'db_config' ] );
+        my $path     = catfile(qw( modules VRTrack Testconfig.pm ));
+        open( my $file, '>', $path ) or die "Cannot write to '$path': $!\n";
+        printf $file <<'END_HERE', $dd->Dump();
 package VRTrack::Testconfig;
 my $db_config;
 %s;
@@ -36,33 +40,38 @@ sub config
 }
 1;
 END_HERE
+    }
 }
 
 sub create_database {
     my ($self, $dbname) = @_;
     my $config          = $self->notes( 'db_config' );
-    my @sql = VRTrack::VRTrack->schema();
-    open(my $mysqlfh, "| mysql -h$config->{host} -u$config->{user} -p$config->{password} -P$config->{port}") || die "could not connect to database for testing\n";
-    print $mysqlfh "drop database if exists $config->{test_db};\n";
-    print $mysqlfh "create database $config->{test_db};\n";
-    print $mysqlfh "use $config->{test_db};\n";
-    foreach my $sql (@sql) {
-        print $mysqlfh $sql;
+    if ($config) {
+        my @sql = VRTrack::VRTrack->schema();
+        open(my $mysqlfh, "| mysql -h$config->{host} -u$config->{user} -p$config->{password} -P$config->{port}") || die "could not connect to database for testing\n";
+        print $mysqlfh "drop database if exists $config->{test_db};\n";
+        print $mysqlfh "create database $config->{test_db};\n";
+        print $mysqlfh "use $config->{test_db};\n";
+        foreach my $sql (@sql) {
+            print $mysqlfh $sql;
+        }
+        close($mysqlfh);
     }
-    close($mysqlfh);
 }
 
 sub ACTION_test {
     my $self   = shift;
     my $config = $self->notes( 'db_config' );
-    eval {
-        $self->create_database( $config->{test_db} );
-    };
-    if ($@){
-        print $@;
-    }
-    else {
-        print "Created database $config->{test_db}\n";
+    if ($config) {
+        eval {
+            $self->create_database( $config->{test_db} );
+        };
+        if ($@){
+            print $@;
+        }
+        else {
+            print "Created database $config->{test_db}\n";
+        }
     }
     $self->SUPER::ACTION_test( @_ );
 }
