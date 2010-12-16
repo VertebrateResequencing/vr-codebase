f09a46261c9bd0d29ef9f1934d15bc6464956ca8
sb10 <sb10@sanger.ac.uk>
2010-12-16 15:53:30 +0000

    now syncs git and svn and updates new set of git checkouts

--- scripts/update_softwarevertres.pl
+++ scripts/update_softwarevertres.pl
@@ -2,14 +2,36 @@
 
 use strict;
 use warnings;
+use Cwd;
 
-#open(my $pipe, "| ssh etch-dev64") || die "failed to open pipe\n";
+$ENV{PERL5LIB} = '';
 
-system("svn update /software/vertres/bin");
-system("svn update /software/vertres/scripts");
-system("svn update /software/vertres/modules");
-#print $pipe "exit\n";
+my $cwd = getcwd;
 
-#close($pipe) || die "failed to close pipe\n";
+# keep svn and git in sync
+my $base = '/software/vertres';
+my $origin_base = "$base/git/origin";
+foreach my $repo ("$origin_base/vr-bin-external", "$origin_base/vr-codebase-svn") {
+    chdir($repo);
+    warn "syncing $repo\n";
+    system("git svn rebase; git svn dcommit");
+}
+
+# update the checkouts that PATH and PERL5LIB point to
+foreach my $repo ("$base/bin-git", "$base/codebase") {
+    chdir($repo);
+    warn "updating $repo\n";
+    system("git pull");
+}
+
+# for now, we'll also update the defunct svn checkouts that nothing should be
+# using
+foreach my $repo ("$base/bin", "$base/scripts", "$base/modules") {
+    chdir($repo);
+    warn "updating $repo\n";
+    system("svn up");
+}
+
+chdir($cwd);
 
 exit;
