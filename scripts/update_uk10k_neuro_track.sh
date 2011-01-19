ccc47b23c013956bd6c09e0f5c736dba3d20cbd0
Jim Stalker <jws@sanger.ac.uk>
2011-01-19 12:47:36 +0000

    Altered uk10k update scripts to use no_fastq  _______________ < FIGHT THE MAN >  ---------------        \   ,__,         \  (oo)____            (__)    )\               ||--|| *

--- scripts/update_uk10k_neuro_track.sh
+++ scripts/update_uk10k_neuro_track.sh
@@ -9,5 +9,5 @@ export ORACLE_HOME=/software/oracle_client-10.2.0
 date="`date +'%y%m%d'`"
 mysqldump -u $VRTRACK_RW_USER -p$VRTRACK_PASSWORD -P$VRTRACK_PORT -h$VRTRACK_HOST vrtrack_uk10k_neuro > "$DUMPS/vrtrack_uk10k_neuro_$date.sql"
 
-update_vrtrack.pl --database vrtrack_uk10k_neuro --projects $CONF/uk10k_neuro_studies --create_individuals > "$ROOT/log/vrtrack_update_uk10k_neuro.log" 2> "$ROOT/log/vrtrack_update_uk10k_neuro.err"
+update_vrtrack.pl --database vrtrack_uk10k_neuro --projects $CONF/uk10k_neuro_studies --create_individuals --no_fastq > "$ROOT/log/vrtrack_update_uk10k_neuro.log" 2> "$ROOT/log/vrtrack_update_uk10k_neuro.err"
 
