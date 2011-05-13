14cff82340268eb49f29e36ce6896ad5e82f8c70
John Maslen <jm23@sanger.ac.uk>
2011-05-13 15:05:01 +0100

    Corrected procedure to alter the lane processed flag and added verbose option

--- scripts/genotype_check_rerun.pl
+++ scripts/genotype_check_rerun.pl
@@ -2,10 +2,11 @@
 
 #
 # genotype_check_rerun.pl --root /path/to/TRACKING --db dbname <file-of-lane-names>
+# (optional --verbose flag can be set to output actions of the script)
 #
 # For example:
 # genotype_check_rerun.pl --root /lustre/scratch106/projects/kuusamo/TRACKING 
-# --db vrtrack_uk10k_cohort lanes.txt
+# --db vrtrack_uk10k_cohort --verbose lanes.txt
 #
 # Sets up the lanes that need to have the genotype checking performed again. It deletes the gtype and 
 # gtypex files and resets the processed flag on the lane table to 1.
@@ -20,14 +21,14 @@ no warnings 'uninitialized';
 use Getopt::Long;
 use VertRes::Utils::VRTrackFactory;
 use VRTrack::Lane;
-use File::Path;
 
-my ($db, $root, $help);
+my ($db, $root, $help, $verbose);
 my @filetypes = ('.gtype', '.gtypex');
 
 GetOptions(
     'd|db=s'        =>  \$db,
     'r|root=s'		=>  \$root,
+    'v|verbose'     =>  \$verbose,
     'h|help'	    =>  \$help,
     );
 
@@ -35,15 +36,18 @@ GetOptions(
     Usage: $0 <file of lanes that need genotype reanalysis performed>  
                 --db        <specify db name>
                 --root      <root directory for the analyses>
+                --verbose   [be extra verbose about what it is doing]
                 --help      <this message>
 
 Restarts the genotype checking for the lanes specified in the file provided.
 
 USAGE
-$db || die "a known --db must be specified\n";
+
 if ('/' ne substr $root,-1,1) {
 	$root = $root.'/';
 }
+die "Root directory $root does not exist\n" unless (-e $root);
+
 my $vrtrack = VertRes::Utils::VRTrackFactory->instantiate(database => $db,
                                                           mode => 'rw');
 unless ($vrtrack){
@@ -53,7 +57,6 @@ unless ($vrtrack){
 # get lanes from file of lane names or stdin
 while (<>){
     my $lanename = $_;
-    my @delfiles;
     chomp $lanename;
     #Check lane actually exists or bail out
     my $lane = VRTrack::Lane->new_by_name($vrtrack,$lanename);
@@ -69,18 +72,14 @@ while (<>){
     #Get file names and delete if they exist
     for my $filetype ( @filetypes ) {
     	my $filepath = $qcdir.$lanename.$filetype;
+    	warn "WARNING: $filepath does not exist\n" unless ( -e $filepath );
     	if ( -e $filepath ) {
-    		unlink $filepath || die "Unable to delete files for $lanename, error = $!\n"; 
-    		print "Deleted: ", $filepath, "\n";
+    		unlink $filepath || die "Unable to delete $filepath, error = $!\n"; 
+    		print "Deleted: ", $filepath, "\n" if ($verbose);
     	}
-    	else {
-       		print "$filepath does not appear to exist - check $lanename details.\n";
-       	}
     }
     #update database
-    my $sql = qq[update lane set processed = 1 where name = ?];
-	my $sth = $vrtrack->{_dbh}->prepare($sql);
-	if ($sth->execute(($lanename))) {
-		print "Processed flag has been reset to 1 for lane ", $lanename, "\n";
-	}
+    $lane->is_processed(qc => 0);
+    $lane->update;
+    print "Processed flag for qc has been reset to 0 for lane ", $lanename, "\n" if ($verbose);
 }
