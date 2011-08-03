48e91a52de200f18bc1d320891b855cbc9ebfb8d
andrewjpage <andrewjpage@gmail.com>
2011-08-03 14:49:11 +0100

    bracket in wrong place

--- modules/VRTrack/Mapstats.pm
+++ modules/VRTrack/Mapstats.pm
@@ -88,8 +88,8 @@ sub fields_dispatch {
                target_bases_20X         => sub { $self->target_bases_20X(@_)},
                target_bases_50X         => sub { $self->target_bases_50X(@_)},
                target_bases_100X        => sub { $self->target_bases_100X(@_)},
-               exome_design_id          => sub { $self->exome_design_id(@_)}),
-               percentage_reads_with_transposon => sub { $self->percentage_reads_with_transposon(@_)};
+               exome_design_id          => sub { $self->exome_design_id(@_)},
+               percentage_reads_with_transposon => sub { $self->percentage_reads_with_transposon(@_)});
 
     return \%fields;
 }
