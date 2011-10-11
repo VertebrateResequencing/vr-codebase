3d92f10fd7bda8f3eac7230b080867a9de147325
andrewjpage <andrewjpage@gmail.com>
2011-10-11 14:53:21 +0100

    over estimate memory

--- modules/VertRes/Utils/Assemblers/velvet.pm
+++ modules/VertRes/Utils/Assemblers/velvet.pm
@@ -167,6 +167,7 @@ sub estimate_memory_required
   my $optimised_params = $self->get_parameters();
   
   my $memory_required = -109635 + (18977*($input_params->{read_length})) + (86326*($input_params->{genome_size})/1000000) + (233353*($input_params->{total_number_of_reads})/1000000) - (51092*$optimised_params->{kmer});
+  $memory_required *= 1.5;
   if($memory_required < 2000000)
   {
     $memory_required = 2000000;
