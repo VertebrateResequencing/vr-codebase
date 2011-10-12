6f029292c72813a5c959a8990c1017dcbcc12325
andrewjpage <andrewjpage@gmail.com>
2011-10-12 12:03:22 +0100

    dont wait for lsf

--- modules/VertRes/Pipelines/Assembly.pm
+++ modules/VertRes/Pipelines/Assembly.pm
@@ -406,7 +406,7 @@ exit;
 
       
 
-      LSF::run($action_lock, $output_directory, $job_name, {bsub_opts => "-n$num_threads -q $queue -M${memory_required_mb}000 -R 'select[mem>$memory_required_mb] rusage[mem=$memory_required_mb]'"}, qq{perl -w $script_name});
+      LSF::run($action_lock, $output_directory, $job_name, {bsub_opts => "-n$num_threads -q $queue -M${memory_required_mb}000 -R 'select[mem>$memory_required_mb] rusage[mem=$memory_required_mb]'", dont_wait=>1}, qq{perl -w $script_name});
 
       # we've only submitted to LSF, so it won't have finished; we always return
       # that we didn't complete
