194273520fcd85fb43c843846824ba80474e0422
andrewjpage <andrewjpage@gmail.com>
2011-10-13 12:02:56 +0100

    missing closing quote

--- modules/VertRes/Utils/Assemblers/velvet.pm
+++ modules/VertRes/Utils/Assemblers/velvet.pm
@@ -75,7 +75,7 @@ sub optimise_parameters_with_reference
 {
   my ($self, $num_threads) = @_;
   my $reference_directory = $self->optimised_directory();
-  `perl $self->{optimiser_exec} -t $num_threads -s $self->{min_kmer} -e $self->{max_kmer} -p 'velvet_optimised_with_reference' -f '-reference -fasta $reference_directory/contigs.fa -shortPaired -bam $reference_directory/contigs.mapped.sorted.bam`;
+  `perl $self->{optimiser_exec} -t $num_threads -s $self->{min_kmer} -e $self->{max_kmer} -p 'velvet_optimised_with_reference' -f '-reference -fasta $reference_directory/contigs.fa -shortPaired -bam $reference_directory/contigs.mapped.sorted.bam'`;
   my $params = $self->get_parameters("velvet_optimised_with_reference_logfile.txt");
   system("mv  $params->{assembly_directory} ".$self->optimised_with_reference_directory());
   system("touch ".$self->optimised_with_reference_directory()."/_velvet_optimised_with_reference_done");
