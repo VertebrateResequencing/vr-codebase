d69c27d51d87efb2256bc2c945fd1d0b2a1cb998
Craig Porter <cp7@sanger.ac.uk>
2012-02-24 16:09:08 +0000

    Added mean depth of coverage and standard deviation to TrackQC_Fastq and Mapping pipelines.  - The TrackQG_Bam pipeline is unaffected, results are only generated for the TrackQC_Fastq pipeline.  - Mean depth of coverage and coverage standard deviation are only output if the get_genome_coverage    option is set in the conf file.

--- modules/VertRes/Utils/Sam.pm
+++ modules/VertRes/Utils/Sam.pm
@@ -3746,4 +3746,49 @@ sub coverage
     return @cover;
 }
 
+=head2 coverage_depth
+
+ Title   : coverage_depth
+ Usage   : my($bases_covered, $depth, $depth_sd) = $obj->coverage_depth($bam_file,$reference_size);
+ Function: Uses samtools pileup to find the number of bases covered, depth of coverage and standard 
+           deviation for depth of coverage.
+ Returns : array with number of bases covered, average depth of coverage and standard deviation for
+           depth of coverage. 
+ Args    : Bam filename and size of reference genome.
+
+=cut
+
+sub coverage_depth
+{
+    my($self, $bam, $ref_size) = @_;
+
+    unless( -e $bam ){ $self->throw("Input file not found: $bam\n"); }
+    unless( $ref_size =~ /^\d+$/ && $ref_size > 0 ){ $self->throw("Reference size must be non-zero integer\n"); }
+
+    my $coverage = 0;
+    my %depth_hist; # Depth from mpileup, key = coverage, value = total_bases.
+
+    # Run pileup. 
+    my $sam = VertRes::Wrapper::samtools->new(verbose => $self->verbose, run_method => 'open', quiet => 1);
+    my $fh = $sam->pileup($bam, undef); 
+    while(my $inp = <$fh>)
+    {
+	my @col = split(/\s+/,$inp,5);
+	$depth_hist{$col[3]}++; # Build histogram
+	$coverage++; # Count bases covered
+    }
+    close $fh;
+    unless( $sam->run_status() ){ $self->throw("Samtools pileup failed for: $bam\n"); }
+    unless( $coverage <= $ref_size ){ $self->throw("Total bases found by pileup exceeds size of reference sequence.\n"); }
+
+    # Add ummapped bases to histogram.
+    $depth_hist{0} = $ref_size - $coverage;
+
+    # Calculate Mean depth and SD
+    my $math_util = VertRes::Utils::Math->new();
+    my %stats = $math_util->histogram_stats(\%depth_hist);
+
+    return($coverage, $stats{'mean'}, $stats{'standard_deviation'});
+}
+
 1;
