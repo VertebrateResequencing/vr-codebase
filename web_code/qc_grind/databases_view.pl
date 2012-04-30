0fa9a8c6dc432e5f36bac38451fc9ebfa2add73c
Chris joyce <cj5@sanger.ac.uk>
2012-04-30 09:31:51 +0100

    specific database order

--- web_code/qc_grind/databases_view.pl
+++ web_code/qc_grind/databases_view.pl
@@ -39,18 +39,44 @@ exit;
 
 sub displayDatabasesPage {
 
+	my $script = $utl->{SCRIPTS}{PROJECTS_VIEW};
+	print qq[ <h2 align="center" style="font: normal 900 1.5em arial">$title</h2> ];
+
+	my @main_dbs = qw (vrtrack_human_wgs vrtrack_human_wes vrtrack_mouse_wgs vrtrack_mouse_wes);
     print qq[
-        <h2 align="center" style="font: normal 900 1.5em arial">$title</h2>
         <div class="centerFieldset">
         <fieldset style="width: 500px">
-        <legend>Databases</legend>
+        <legend>Main Databases</legend>
     ];
-    
-    my @dbs = VertRes::Utils::VRTrackFactory->databases(1);
+    foreach( @main_dbs ) {
+		print $cgi->p("<a href='$script?db=$_'> $_ </a>");
+    }
+    print qq[ </fieldset> </div> ];
 
-	my $script = $utl->{SCRIPTS}{PROJECTS_VIEW};
-	
+	my @uk10k_dbs = qw (vrtrack_uk10k_cohort vrtrack_uk10k_neuro vrtrack_uk10k_obesity vrtrack_uk10k_rare);
+    print qq[
+        <div class="centerFieldset">
+        <fieldset style="width: 500px">
+        <legend>UK10K Databases</legend>
+    ];
+    foreach( @uk10k_dbs ) {
+		print $cgi->p("<a href='$script?db=$_'> $_ </a>");
+    }
+    print qq[ </fieldset> </div> ];
+
+	my %done;
+	push (@main_dbs, @uk10k_dbs);
+	foreach (@main_dbs) {
+		$done{$_}++;
+	}
+    my @dbs = VertRes::Utils::VRTrackFactory->databases(1);
+    print qq[
+        <div class="centerFieldset">
+        <fieldset style="width: 500px">
+        <legend>Other Databases</legend>
+    ];
     foreach( @dbs ) {
+		next if $done{$_};
 		print $cgi->p("<a href='$script?db=$_'> $_ </a>");
     }
     print qq[ </fieldset> </div> ];
