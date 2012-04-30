b74f443fe5d9d08c459f133ae20484eb3c02ed17
Chris joyce <cj5@sanger.ac.uk>
2012-04-30 09:35:51 +0100

    lane status filter

--- web_code/qc_grind/study_lanes.pl
+++ web_code/qc_grind/study_lanes.pl
@@ -3,7 +3,6 @@
 # Displays Study Lane QC status information with update form
 #
 # Author:        cj5
-# Maintainer:    cj5
 # Created:       2012-03-28
 
 BEGIN {
@@ -39,6 +38,8 @@ my $lane_view_script = $utl->{SCRIPTS}{LANE_VIEW};
 
 my $USER = $sw->username();
 my $cgi = $sw->cgi();
+my $form_submission = ($cgi->param('filter') or $cgi->param('update')) ? 1 : 0;
+
 my $SCRIPT_NAME = $cgi->url(-relative=>1);
 
 my $db = $cgi->param('db');
@@ -75,7 +76,7 @@ sub displayProjectLaneForm
 
 	my ($gt_status_filt, $npg_status_filt, $auto_qc_status_filt, $bases_mapped_filt, $duplication_filt, $rmdup_mapped_filt);
 
-	if( $cgi->param('filter')) {
+	if ($form_submission) {
 		$gt_status_filt = $cgi->param('gt_status');
 		$npg_status_filt = $cgi->param('npg_status');
 		$auto_qc_status_filt = $cgi->param('auto_qc_status');
@@ -88,7 +89,7 @@ sub displayProjectLaneForm
 		$npg_status_filt = 'all';
 		$auto_qc_status_filt = 'all';
 	}
-    
+   
     my $samples = $project->samples();
     $utl->displayError( "Cant get samples for project: $projectID",$sw ) unless $samples;
     
@@ -108,8 +109,7 @@ sub displayProjectLaneForm
 	print $cgi->h2({-align=>"center", -style=>"font: normal 900 1.5em arial"},"<a href='$main_script'>QC Grind</a> Lane Status Update");
 	print $cgi->h3({-align=>"center", -style=>"font: normal 700 1.5em arial"},"Database : <a href='$proj_view_script?db=$database'>$database</a>");
 	print $cgi->h3({-align=>"center", -style=>"font: normal 700 1.5em arial"},"Project : $pname");
-	print $cgi->h5({-style=>"font: arial"},"<a href='$pending_view?mode=2&amp;db=$database'>Pending Requests</a>");
-
+	print $cgi->h5({-style=>"font: arial"},"<a  target='_blank' href='$pending_view?mode=2&amp;db=$database'>Pending Requests</a>");
     print $cgi->br;
 
     print qq[
@@ -124,6 +124,7 @@ sub displayProjectLaneForm
 		<th style="width: 40px">GTPend</th>
 		<th style="width: 40px">Pend</th>
         <th>Individual</th>
+        <th>Sample</th>
         <th>Library</th>
         <th>Run</th>
     	<th>Genotype</th>
@@ -140,9 +141,15 @@ sub displayProjectLaneForm
     print $cgi->hidden("db","$database");
     print $cgi->hidden("proj_id","$projectID");
 
-    print qq[ <tr> ];
-	print $cgi->th(['','','','','','','','']);
-	print "<th>", $cgi->popup_menu( -name => 'gt_status', -values => ['all','confirmed','unconfirmed','unchecked','wrong'], -default => 'all',), "</th>"; 
+    print qq[ <tr style="background-color:#F5F5F5"> ];
+
+	my @lane_status = qw (passed failed investigate gt_pending pending);
+	foreach my $status (@lane_status) {
+        print $cgi->th( $cgi->checkbox(-name=>$status, -checked=>1, -label=>''));
+	}
+
+	print $cgi->th(['','','','']);
+	print "<th>", $cgi->popup_menu( -name => 'gt_status', -values => ['all','confirmed','unconfirmed','unchecked','unknown','candidate','wrong'], -default => 'all',), "</th>"; 
 	print "<th>", $cgi->popup_menu( -name => 'npg_status', -values => ['all','pass','fail','pending'], -default => 'all',), "</th>"; 
 	print "<th>", $cgi->popup_menu( -name => 'auto_qc_status', -values => ['all','passed','failed','no_qc'], -default => 'all',), "</th>"; 
 	print "<th>", $cgi->textfield(-name=>'bases_mapped', -size=>1), "</th>";
@@ -157,6 +164,7 @@ sub displayProjectLaneForm
 
 		foreach my $sample ( @samples ) {
 
+			my $sample_name = $sample->name;
 			my @libraries = sort {$a->name cmp $b->name} @{$sample->libraries()};
 
 			foreach my $library ( @libraries ) {
@@ -165,6 +173,11 @@ sub displayProjectLaneForm
 				my $lanes = $library->lanes();
 
 				foreach my $lane (sort {$a->name cmp $b->name} @$lanes ) {
+
+					if ($form_submission) { # filter on lane status check button
+						next unless $cgi->param($lane->qc_status);
+					}
+
 					my $mapping_raw_bases = 0;
 					my $duplication = 0;
 
@@ -193,21 +206,24 @@ sub displayProjectLaneForm
         				my $lane_qc_status = $lane->qc_status;
 						my $lane_status_colour = $utl->get_colour_for_status($lane_qc_status);
 
+						my $disabled = $utl->{AUTH_USERS}{$USER} ? '' : 'DISABLED';
 						foreach my $status ( qw (passed failed investigate gt_pending pending)) {
 							my $state =  $lane_qc_status eq $status ? 'checked' : '';
-							print $cgi->td("<input type='radio' name='$lane_id' value='$status' $state >");
+							print $cgi->td("<input type='radio' name='$lane_id' value='$status' $state $disabled>");
 						}
 
 						my $gt_found = $lane_mapstats->{genotype_found};
 						my $gt_ratio = $lane_mapstats->{genotype_ratio};
-						my $gt_display = "$gt_status ($gt_found:$gt_ratio)";
+						my $gt_display = $gt_status;
+						$gt_display .= " ($gt_found:$gt_ratio)" if $gt_found;
+						my $gt_status_colour = get_gt_status_colour($gt_status,$sample_name,$gt_found);
 
 						print $cgi->td($iname);
+						print $cgi->td({-style=>"background-color:#F5F5F5"},$sample_name);
 						print $cgi->td($libname);
 
 						print qq [ <td style="background-color:$lane_status_colour;"><a href="$lane_view_script?lane_id=$lane_id&amp;db=$database">$lanename</a></td> ];
-
-						print $cgi->td($gt_display);
+						print qq [ <td style="background-color:$gt_status_colour;">$gt_display</td> ];
 						print $cgi->td([$npg_qc, $auto_qc_status, $lane_mapstats->{mapping_raw_bases}, $lane_mapstats->{duplication}, $lane_mapstats->{rmdup_bases_mapped}]);
 						print qq[</tr>];
 					}
@@ -281,13 +297,14 @@ sub downloadLaneData {
         else{$individuals2Samples{ $indname } = [ $sample ];}
     }
 
-    print join ("\t","Sanger ID/Source Sample Name","Library","Run number","Genotype","Lane QC status","NPG status","Auto QC status","Mapped","Duplication","RmDup Mapped"), "\n";
+    print join ("\t","Sanger ID","Sample Name","Library","Run number","Genotype","Lane QC status","NPG status","Auto QC status","Mapped","Duplication","RmDup Mapped"), "\n";
 
     foreach( sort( keys( %individuals2Samples ) ) ) {
         my $iname = $_;
         my @samples = @{$individuals2Samples{ $_ }};
 
 		foreach my $sample ( @samples ) {
+			my $sample_name = $sample->name;
 			my @libraries = sort {$a->name cmp $b->name} @{$sample->libraries()};
 
 			foreach my $library ( @libraries ) {
@@ -297,6 +314,9 @@ sub downloadLaneData {
 				my $lanes = $library->lanes();
 
 				foreach my $lane (sort {$a->name cmp $b->name} @$lanes ) {
+
+					next unless $cgi->param($lane->qc_status); # filter on lane status check button
+
 					my $mapping_raw_bases = 0;
 					my $duplication = 0;
 
@@ -315,16 +335,16 @@ sub downloadLaneData {
 
 						my $gt_found = $lane_mapstats->{genotype_found};
 						my $gt_ratio = $lane_mapstats->{genotype_ratio};
-						my $gt_display = "$gt_status ($gt_found:$gt_ratio)";
+						my $gt_display = $gt_status;
+						$gt_display .= " ($gt_found:$gt_ratio)" if $gt_found;
 
 						next if $bases_mapped_filt && $lane_mapstats->{mapping_raw_bases} < $bases_mapped_filt;
 						next if $duplication_filt && $lane_mapstats->{duplication} < $duplication_filt;
 						next if $rmdup_mapped_filt && $lane_mapstats->{rmdup_bases_mapped} < $rmdup_mapped_filt;
 
-						print (join("\t",$iname,$libname,$lanename,$gt_display,$lane_qc_status,$npg_qc,$auto_qc_status,$lane_mapstats->{mapping_raw_bases},$lane_mapstats->{duplication},$lane_mapstats->{rmdup_bases_mapped}),"\n");
+						print (join("\t",$iname,$sample_name,$libname,$lanename,$gt_display,$lane_qc_status,$npg_qc,$auto_qc_status,$lane_mapstats->{mapping_raw_bases},$lane_mapstats->{duplication},$lane_mapstats->{rmdup_bases_mapped}),"\n");
 					}
 				} # foreach lane
-
 			} # foreach lib
 		} # foreach sample
 	}
@@ -342,10 +362,41 @@ sub getMapStats {
             $lane_mapstats{duplication} = sprintf("%.1f", ($mapstats->rmdup_bases_mapped()/$mapstats->raw_bases)*100);
             $lane_mapstats{rmdup_bases_mapped} =  sprintf("%.1f", ($mapstats->rmdup_bases_mapped/1000000000)); # GB
             $lane_mapstats{genotype_found} = $mapstats->genotype_found;
-            $lane_mapstats{genotype_ratio} = sprintf("%.3f",$mapstats->genotype_ratio);
+            $lane_mapstats{genotype_ratio} = sprintf("%.3f",$mapstats->genotype_ratio) if $mapstats->genotype_ratio;
             last;
         }
     }
     return \%lane_mapstats;
+}
+
+sub get_gt_status_colour
+{
+	my (($status,$iname,$gt_found)) = @_;
+
+	my $green = "#CCFFCC";
+	my $red = "#FFC0C0";
+	my $status_colour;
+
+	if ($status eq 'confirmed') {
+		$status_colour=$green;
+	}
+	elsif ($status eq 'wrong') {
+		$status_colour=$red;
+	}
+	elsif ($status eq 'unknown') {
+		$status_colour=$red;
+	}
+	elsif ($gt_found) {
+		if ($gt_found eq $iname) {
+			$status_colour=$green;
+		}
+		else {
+			$status_colour=$red;
+		}
+	}
+	else {
+		$status_colour="#F5F5F5";
+	}
+	return $status_colour;
 }
 
