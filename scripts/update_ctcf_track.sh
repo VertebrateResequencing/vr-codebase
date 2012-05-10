b573f1d1bdc516ea2ad899ff283620efc510321e
John Maslen <jm23@sanger.ac.uk>
2012-05-10 12:39:51 +0100

    correcting file name string in shell script

--- scripts/update_ctcf_track.sh
+++ scripts/update_ctcf_track.sh
@@ -10,8 +10,8 @@ export LD_LIBRARY_PATH=/software/badger/lib:/software/oracle_client-10.2.0/lib
 export ORACLE_HOME=/software/oracle_client-10.2.0
 
 date="`date +'%y%m%d'`"
-mysqldump -u $VRTRACK_RW_USER -p$VRTRACK_PASSWORD -P$VRTRACK_PORT -h$VRTRACK_HOST $DB > "$DUMPS/$DB_$date.sql"
+mysqldump -u $VRTRACK_RW_USER -p$VRTRACK_PASSWORD -P$VRTRACK_PORT -h$VRTRACK_HOST $DB > $DUMPS/$DB"_"$date.sql
 
-$COMPRESS_CMD $DUMPS/$DB_$date.sql
+$COMPRESS_CMD $DUMPS/$DB"_"$date.sql
 
 update_vrtrack.pl --database $DB --projects $STUDY --create_individuals --no_fastq > "$ROOT/log/update_$DB.log" 2> "$ROOT/log/update_$DB.err"
--- scripts/update_vrtrack_mouse_wes.sh
+++ scripts/update_vrtrack_mouse_wes.sh
@@ -11,9 +11,9 @@ export LD_LIBRARY_PATH=/software/badger/lib:/software/oracle_client-10.2.0/lib
 export ORACLE_HOME=/software/oracle_client-10.2.0
 
 date="`date +'%y%m%d'`"
-mysqldump -u $VRTRACK_RW_USER -p$VRTRACK_PASSWORD -P$VRTRACK_PORT -h$VRTRACK_HOST $DB > "$DUMPS/$DB_$date.sql"
+mysqldump -u $VRTRACK_RW_USER -p$VRTRACK_PASSWORD -P$VRTRACK_PORT -h$VRTRACK_HOST $DB > $DUMPS/$DB"_"$date.sql
 
-$COMPRESS_CMD $DUMPS/$DB_$date.sql
+$COMPRESS_CMD $DUMPS/$DB"_"$date.sql
 
 $BIN_EXT/update_pipeline.pl -s $CONF/$DB_studies -d $DB -v
 
