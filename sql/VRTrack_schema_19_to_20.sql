81d58ebc96d2b3b20665ff37f45bf1271eecbd86
Sendu Bala <sb10@sanger.ac.uk>
2012-07-10 15:03:58 +0100

    fixed create table statement to remove line breaks

--- sql/VRTrack_schema_19_to_20.sql
+++ sql/VRTrack_schema_19_to_20.sql
@@ -11,16 +11,6 @@ ALTER TABLE `seq_tech` MODIFY `name` varchar(255);
 ALTER TABLE `submission` MODIFY `name` varchar(255);
 
 DROP TABLE IF EXISTS `autoqc`;
-CREATE TABLE `autoqc`
-(
-  `autoqc_id` mediumint(8) unsigned NOT NULL auto_increment,
-   mapstats_id mediumint(8) unsigned NOT NULL DEFAULT 0,
-   test varchar(50) NOT NULL default '',
-   result tinyint(1) DEFAULT 0,
-   reason varchar(200) NOT NULL default '',
-   PRIMARY KEY (`autoqc_id`),
-  KEY  `mapstats_id` (`mapstats_id`),
-   UNIQUE KEY `mapstats_test` (`mapstats_id`, `test`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `autoqc` (`autoqc_id` mediumint(8) unsigned NOT NULL auto_increment, mapstats_id mediumint(8) unsigned NOT NULL DEFAULT 0, test varchar(50) NOT NULL default '', result tinyint(1) DEFAULT 0, reason varchar(200) NOT NULL default '', PRIMARY KEY (`autoqc_id`), KEY  `mapstats_id` (`mapstats_id`), UNIQUE KEY `mapstats_test` (`mapstats_id`, `test`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 update schema_version set schema_version=20;
