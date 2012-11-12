61212e8d8c763905d0fea9fd3fe13a4b3a26f0a5
Sendu Bala <sb10@sanger.ac.uk>
2012-11-12 14:09:09 +0000

    rsync things not under version control

--- scripts/update_softwarevertres.pl
+++ scripts/update_softwarevertres.pl
@@ -13,5 +13,12 @@ foreach my $repo ("$base/bin-external", "$base/codebase", "$base/vrpipe/master")
     }
 }
 
+# rsync things from 'main' /software/vertres to precise-only /software/vertres
+my @to_rsync = qw(/software/vertres/etc/ /software/vertres/vrpipe/archive_disc_pool);
+foreach my $path (@to_rsync) {
+    warn "\nrsyncing $path\n";
+    system("rsync -rlptD -z --delete $path precise-dev64:$path");
+}
+
 exit;
 
