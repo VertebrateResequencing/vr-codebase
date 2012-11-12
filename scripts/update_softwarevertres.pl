b5f52223101d19cfc4932b84b61e7fd1b24e89f9
Sendu Bala <sb10@sanger.ac.uk>
2012-11-12 12:48:15 +0000

    removed extraneous cruft and now have it git pull on normal and precise servers

--- scripts/update_softwarevertres.pl
+++ scripts/update_softwarevertres.pl
@@ -2,22 +2,16 @@
 
 use strict;
 use warnings;
-use Cwd;
-
-$ENV{PERL5LIB} = '';
-
-my $cwd = getcwd;
 
 my $base = '/software/vertres';
 
 # update the checkouts that PATH and PERL5LIB point to
 foreach my $repo ("$base/bin-external", "$base/codebase", "$base/vrpipe/master") {
-    chdir($repo);
-    warn "\nupdating $repo\n";
-    system("umask 002; git checkout .; git pull; umask 007");
+    foreach my $server ('uk10k-login', 'precise-dev64') {
+        warn "\nupdating $repo on $server\n";
+        system(qq[ssh $server "umask 002; cd $repo; git checkout .; git pull"]);
+    }
 }
-
-chdir($cwd);
 
 exit;
 
