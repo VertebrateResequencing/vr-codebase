4a54dc7fea5b66c60b63086ee4b1fd2cc239eec1
nds <nds@sanger.ac.uk>
2013-06-13 10:06:13 +0100

    corrected wrong variable name

--- scripts/delete_vrtrack_lanes.pl
+++ scripts/delete_vrtrack_lanes.pl
@@ -92,7 +92,7 @@ while (<>){
         	
         	#Clear up any files in the FSU FILE EXISTS tables
         	my $fsu = VertRes::Utils::FileSystem->new(reconnect_db=>1);
-    		$fsu->file_exists(abs_path($lane_dir) , recurse =>1, wipe_out=>1);
+    		$fsu->file_exists(abs_path($lanedir) , recurse =>1, wipe_out=>1);
  
  	        my $stored_path = readlink $lanedir;  #Get the folder pointed to by the symlink
 
