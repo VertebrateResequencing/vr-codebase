36bc56cd8a72f34e94ab01763324d478059baa10
John Maslen <jm23@sanger.ac.uk>
2013-09-23 11:31:31 +0100

    team page for new vm, plus updated ViewUtil module

--- modules/VertRes/QCGrind/ViewUtil.pm
+++ modules/VertRes/QCGrind/ViewUtil.pm
@@ -6,6 +6,13 @@ use DBI;
 use strict;
 
 sub new {
+	
+    $ENV{VRTRACK_HOST} = 'mcs10';
+    $ENV{VRTRACK_PORT} = 3306;
+    $ENV{VRTRACK_RO_USER} = 'vreseq_ro';
+    $ENV{VRTRACK_RW_USER} = 'vreseq_rw';
+    $ENV{VRTRACK_PASSWORD} = 't3aml3ss';	
+	
     my $self={};
 
 	$self->{SCRIPTS} = {
@@ -26,21 +33,33 @@ sub new {
 		QCGRIND_LANE              => '../qc_grind/lane_view.pl',
 		QCGRIND_SAMPLES           => '../qc_grind/samples_view.pl',
 		INDEX_PAGE                => '../index.pl',
+		HIPSCI_COHORTS_VIEW       => 'cohorts_view.pl',
+		HIPSCI_SUMMARY_VIEW       => 'cohort_summary_view.pl',
+		HIPSCI_DETAILED_VIEW      => 'cohort_detailed_view.pl',		
 	};
 	
 	
 	$self->{SQL} = {
-	    PENDING_LIB              => "select p.name, s.name, r.prep_status, r.changed, r.ssid from study y, latest_project p, latest_sample s, latest_library_request r where y.study_id = p.study_id and p.project_id = s.project_id and s.sample_id = r.sample_id and r.prep_status in ('pending', 'started') and y.name = ? order by p.name, r.ssid",
-	    PENDING_SEQ              => "select p.name, s.name, r.seq_status, r.changed, r.ssid from study y, latest_project p, latest_sample s, latest_library l, latest_seq_request r where y.study_id = p.study_id and p.project_id = s.project_id and s.sample_id = l.sample_id and l.library_id = r.library_id and r.seq_status in ('pending', 'started') and y.name = ? order by p.name, r.ssid",
-	    PENDING_MULTIPLEX_SEQ    => "select p.name, s.name, r.seq_status, r.changed, r.ssid from study y, latest_project p, latest_sample s, latest_library l, library_multiplex_pool m, latest_seq_request r where y.study_id = p.study_id and p.project_id = s.project_id and s.sample_id = l.sample_id and l.library_id = m.library_id and m.multiplex_pool_id = r.multiplex_pool_id and r.seq_status in ('pending', 'started') and y.name = ?",
+	    PENDING_LIB              => "select distinct p.name, s.name, r.prep_status, r.changed, r.ssid from study y, latest_project p, latest_sample s, latest_library_request r where y.study_id = p.study_id and p.project_id = s.project_id and s.sample_id = r.sample_id and r.prep_status in ('pending', 'started') and y.name = ? order by p.name, r.ssid",
+	    PENDING_SEQ              => "select distinct p.name, s.name, r.seq_status, r.changed, r.ssid from study y, latest_project p, latest_sample s, latest_library l, latest_seq_request r where y.study_id = p.study_id and p.project_id = s.project_id and s.sample_id = l.sample_id and l.library_id = r.library_id and r.seq_status in ('pending', 'started') and y.name = ? order by p.name, r.ssid",
+	    PENDING_MULTIPLEX_SEQ    => "select distinct p.name, s.name, r.seq_status, r.changed, r.ssid from study y, latest_project p, latest_sample s, latest_library l, library_multiplex_pool m, latest_seq_request r where y.study_id = p.study_id and p.project_id = s.project_id and s.sample_id = l.sample_id and l.library_id = m.library_id and m.multiplex_pool_id = r.multiplex_pool_id and r.seq_status in ('pending', 'started') and y.name = ?",
 	    DISK_USAGE_DIRECTORY     => "select path_root, file_type, s, s_gb, file_count from vrpipe_root_top_level where s_gb > 0 and top_level_display = 1 order by s_gb desc",
 	    DISK_USAGE_PIPELINE      => "select ps_id, ps_name, ps_user, ps_type, total_s_gb, file_count from vrpipe_usage_total where total_s_gb > 0 order by total_s_gb desc",
 	    DISK_USAGE_INDIV_DIR     => "select ps_id, ps_name, ps_user, ps_type, file_type, path_root, s_gb, file_count from vrpipe_usage_top_level where path_root = ? and s_gb > 0 order by s_gb desc", 
 	    DISK_USAGE_INDIV_PIPE    => "select ps_id, ps_name, ps_user, ps_type, file_type, path_root, s_gb, file_count from vrpipe_usage_top_level where ps_id = ? and s_gb > 0 order by s_gb desc", 
 	    DISK_USAGE_FILES         => "select ps_id, ps_name, ps_user, ps_type, s, type, path from vrpipe_file_info where ps_id = ? and path_root = ? and type = ? order by s desc",
+	    HIPSCI_FETCH_COHORT      => "SELECT name from individual",
+	    HIPSCI_FETCH_SAMPLES     => "SELECT s.name, s.note_id from latest_sample s, individual i where s.individual_id = i.individual_id and i.name = ?",
+	    HIPSCI_FETCH_CNV_TOTALS  => "SELECT distinct s.name, m.raw_bases, m.clip_bases, m.gt_expected from latest_mapstats m, latest_lane l, latest_library b, latest_sample s, individual i where m.lane_id=l.lane_id and l.library_id=b.library_id and b.sample_id=s.sample_id and  s.individual_id = i.individual_id and i.name = ?",
+	    HIPSCI_FETCH_LANE_ID     => "SELECT min(l.lane_id) from latest_lane l, latest_library b, latest_sample s, individual i where l.library_id = b.library_id and b.sample_id = s.sample_id and s.individual_id = i.individual_id and i.name = ?",
     };
-
-
+    
+    $self->{VRTRACK_DATABASES} = {
+		GENOTYPING => 'vrtrack_hipsci_qc1_genotyping_webtrial',
+		EXPRESSION => 'vrtrack_hipsci_qc1_expression_webtrial',
+		WEB_TABLES => 'vrtrack_web_tables',
+	};
+	
     $self->{CSS} = <<CSS ;
 
 .centerFieldset {
@@ -175,7 +194,7 @@ padding: 2px;
 
 .thumbnail:hover span{ /*CSS for enlarged image on hover*/
 visibility: visible;
-top: 0;
+top: -600px;
 left: -480px; /*position where enlarged image should offset horizontally */
 
 }
@@ -185,13 +204,31 @@ CSS
     return $self;
 }
 
+sub printPreviewImage
+{
+	my $self = shift;
+    my $imageObj = shift;
+    
+    $imageObj->name =~ /\.(\w+)$/;
+    my $type = $1;
+    my $uri = URI->new("data:");
+    $uri->media_type("image/$type");
+    $uri->data($imageObj->image);
+    my $caption = $imageObj->caption;
+    
+    print qq[
+        <a class="thumbnail" href="#">
+        <img src="$uri" width="100" height="100" alt="$caption" style="border:1px dotted #83A4C3;">
+        <span><img src="$uri" alt="$caption" style="border:5px solid #83A4C3;"/></span></a>
+    ];
+}
 
 sub displayDatabasesPage {
     my ($self,$title,$cgi,$script,$alldb,$pending) = @_;
 	my $index = $self->{SCRIPTS}{INDEX_PAGE};
 	print qq[ <h4 align="center" style="font: arial"><i><a href="$index">Team 145</a></i> : $title</h4> ];
     my $pending_db = 'vrtrack_pending_requests';
-    my @main_dbs = qw (vrtrack_human_wgs vrtrack_human_wes vrtrack_human_wes_v5 vrtrack_mouse_wgs vrtrack_mouse_wes vrtrack_mouse_wes_GRCm38 vrtrack_mouse_wgs_GRCm38 vrtrack_scerevisiae_wgs);
+    my @main_dbs = qw (vrtrack_mouse_wes_GRCm38 vrtrack_mouse_wgs_GRCm38 vrtrack_human_wes_v5 vrtrack_human_wgs vrtrack_human_wes vrtrack_mouse_wgs vrtrack_mouse_wes vrtrack_scerevisiae_wgs);
     print qq[
         <div class="centerFieldset">
         <fieldset style="width: 500px">
@@ -203,7 +240,7 @@ sub displayDatabasesPage {
     }
     print qq[ </fieldset> </div> ];
 
-	my @uk10k_dbs = qw (vrtrack_uk10k_cohort vrtrack_uk10k_neuro vrtrack_uk10k_obesity vrtrack_uk10k_rare vrtrack_uk10k_obesity_wga vrtrack_uk10k_rare_wga);
+	my @uk10k_dbs = qw (vrtrack_uk10k_obesity_wga vrtrack_uk10k_rare_wga vrtrack_uk10k_cohort vrtrack_uk10k_neuro vrtrack_uk10k_obesity vrtrack_uk10k_rare);
     print qq[
         <div class="centerFieldset">
         <fieldset style="width: 500px">
@@ -229,7 +266,7 @@ sub displayDatabasesPage {
 		@dbs = grep(!/irods/, @dbs);
 		@dbs = grep(!/kuusamo/, @dbs);
 		@dbs = grep(!/requests/, @dbs);
-		@dbs = grep(!/web_index/, @dbs);
+		@dbs = grep(!/web_tables/, @dbs);
     	print qq[
         	<div class="centerFieldset">
         	<fieldset id="fieldset1" class="coolfieldset" style="width: 500px">
@@ -273,9 +310,8 @@ sub getDatabaseID
 {
 	my ($self, $db) = @_;
 	my $db_id;
-    my $web_db = 'vrtrack_web_index';
-	my $vrtrack = $self->connectToDatabase($web_db);
-	$self->displayError( "Failed to connect to web database: $web_db" ) unless defined( $vrtrack );
+	my $vrtrack = $self->connectToDatabase($self->{VRTRACK_DATABASES}{WEB_TABLES});
+	$self->displayError( "Failed to connect to vrtrack web database" ) unless defined( $vrtrack );
 	my $sql = qq[SELECT db_id FROM tracking_database where db_name = ?];
 	my $sth = $vrtrack->{_dbh}->prepare($sql);
 	if ($sth->execute($db)) {
@@ -292,9 +328,8 @@ sub getSampleMappings
 {
 	my ($self, $db_id, $pid) = @_;
 	my %mappings;
-    my $web_db = 'vrtrack_web_index';
-	my $vrtrack = $self->connectToDatabase($web_db);
-	$self->displayError( "Failed to connect to web database: $web_db" ) unless defined( $vrtrack );
+	my $vrtrack = $self->connectToDatabase($self->{VRTRACK_DATABASES}{WEB_TABLES});
+	$self->displayError( "Failed to connect to vrtrack web database" ) unless defined( $vrtrack );
 	my $sql = qq[SELECT supplier_name, accession_number, sanger_sample_name FROM sample_id_mapping where db_id = ?];
 	$sql = $sql." and project_id = ?" unless $pid == 99999;
 	my $sth = $vrtrack->{_dbh}->prepare($sql);	
@@ -314,9 +349,8 @@ sub fetchProjectName
 {
 	my ($self, $db_id, $pid) = @_;
     my $pname;
-    my $web_db = 'vrtrack_web_index';
-	my $vrtrack = $self->connectToDatabase($web_db);
-	$self->displayError( "Failed to connect to web database: $web_db" ) unless defined( $vrtrack );
+	my $vrtrack = $self->connectToDatabase($self->{VRTRACK_DATABASES}{WEB_TABLES});
+	$self->displayError( "Failed to connect to vrtrack web database" ) unless defined( $vrtrack );
 	my $sql = qq[SELECT project_name FROM db_projects where project_id = ? and db_id =?];
 	my $sth = $vrtrack->{_dbh}->prepare($sql);
 	if ($sth->execute($pid, $db_id)) {
@@ -332,9 +366,8 @@ sub fetchProjectName
 sub fetchUsageTotals
 {
 	my ($self, $sql) = @_;
-    my $web_db = 'vrtrack_web_index';
-	my $vrtrack = $self->connectToDatabase($web_db);
-	$self->displayError( "Failed to connect to web database: $web_db" ) unless defined( $vrtrack );	
+	my $vrtrack = $self->connectToDatabase($self->{VRTRACK_DATABASES}{WEB_TABLES});
+	$self->displayError( "Failed to connect to vrtrack web database" ) unless defined( $vrtrack );
 	my $sth = $vrtrack->{_dbh}->prepare($sql);
 	my @totals;
 	if ($sth->execute()) {
@@ -348,9 +381,8 @@ sub fetchUsageTotals
 sub fetchIndividualUsage
 {
 	my ($self, $sql, $param1, $param2, $param3) = @_;
-    my $web_db = 'vrtrack_web_index';
-	my $vrtrack = $self->connectToDatabase($web_db);
-	$self->displayError( "Failed to connect to web database: $web_db" ) unless defined( $vrtrack );	
+	my $vrtrack = $self->connectToDatabase($self->{VRTRACK_DATABASES}{WEB_TABLES});
+	$self->displayError( "Failed to connect to vrtrack web database" ) unless defined( $vrtrack );	
 	my $sth = $vrtrack->{_dbh}->prepare($sql);
 	my @totals;
 	if ( $param2 && $param3 ) {
@@ -371,6 +403,83 @@ sub nonVrtrackConnection
     my $dbh = DBI->connect("dbi:mysql:$db;host=$ENV{VRTRACK_HOST};port=$ENV{VRTRACK_PORT}", $ENV{VRTRACK_RO_USER}, undef, { 'RaiseError' => 1 } );
     $self->displayError( "Failed to connect to non-VRTrack database: $db" ) unless defined( $dbh );
     return $dbh;
+}
+
+sub getHipsciCohorts
+{
+	my ($self, $db) = @_;
+    my $vrtrack = $self->connectToDatabase($db);
+	$self->displayError( "Failed to connect to database: $db" ) unless defined( $vrtrack );
+	my $sql = $self->{SQL}{HIPSCI_FETCH_COHORT};
+	my $sth = $vrtrack->{_dbh}->prepare($sql);
+	my @cohorts;
+	if ($sth->execute()) {
+		while (my ($cohort) = $sth->fetchrow_array()) {
+			push @cohorts, $cohort;
+		}
+    }
+    return @cohorts;
+}
+
+sub getGenotypingSamples 
+{
+	my ($self, $db, $cohort) = @_;
+	my %samples;
+	my $vrtrack = $self->connectToDatabase($db);
+	$self->displayError( "Failed to connect to database: $db" ) unless defined( $vrtrack );
+	my $sql = $self->{SQL}{HIPSCI_FETCH_SAMPLES};
+	my $sth = $vrtrack->{_dbh}->prepare($sql);	
+	$sth->execute($cohort);
+	while (my ($sample, $control) = $sth->fetchrow_array()) {
+		$samples{$sample} = $control;
+	}
+	return %samples;	
+}
+
+sub getExpressionLaneID
+{
+	my ($self, $db, $cohort) = @_;
+	my $vrtrack = $self->connectToDatabase($db);
+	$self->displayError( "Failed to connect to database: $db" ) unless defined( $vrtrack );
+	my $sql = $self->{SQL}{HIPSCI_FETCH_LANE_ID};
+	my $sth = $vrtrack->{_dbh}->prepare($sql);	
+	$sth->execute($cohort);
+	my $lane_id;
+	while (my ($id) = $sth->fetchrow_array()) {
+		$lane_id = $id;
+	}
+    return $lane_id;	
+}
+
+sub getGenotypingCNVTotals 
+{
+	my ($self, $db, $cohort) = @_;
+	my %cnv;
+	my $vrtrack = $self->connectToDatabase($db);
+	$self->displayError( "Failed to connect to database: $db" ) unless defined( $vrtrack );
+	my $sql = $self->{SQL}{HIPSCI_FETCH_CNV_TOTALS};
+	my $sth = $vrtrack->{_dbh}->prepare($sql);	
+	$sth->execute($cohort);
+	while (my ($sample, $intersection, $diff, $cnv_type) = $sth->fetchrow_array()) {
+		push @{ $cnv{$sample}{$cnv_type} }, ($intersection, $diff);
+	}
+	return %cnv;	
+}
+
+sub getGenotypingCNVLeastValue
+{
+	my ($self, $db, $cohort, $cnv_flag) = @_;
+	my %cnv;
+	my $vrtrack = $self->connectToDatabase($db);
+	$self->displayError( "Failed to connect to database: $db" ) unless defined( $vrtrack );
+	my $sql = $self->{SQL}{HIPSCI_FETCH_CNV_TOTALS};
+	my $sth = $vrtrack->{_dbh}->prepare($sql);	
+	$sth->execute($cohort);
+	my $least = -1;
+	while (my ($sample, $intersection, $diff, $cnv_type) = $sth->fetchrow_array()) {
+		$least = $diff if $cnv_type eq $cnv_flag && ( $least < 0 || $least > $diff);
+	}
+    return $least;	
 }
 
 1;
--- web_code/index.pl
+++ web_code/index.pl
@@ -3,7 +3,8 @@ use strict;
 use warnings;
 use URI;
 
-use SangerPaths qw(core team145);
+use lib '../lib';
+use SangerPaths;
 use SangerWeb;
 use VertRes::QCGrind::ViewUtil;
 
@@ -28,15 +29,17 @@ print qq[<div class="centerFieldset">
         <fieldset style="width: 550px">
             <legend>Select web tool</legend>
     
-            <p><a href="qc_grind/databases_view.pl">QC Grind</a><br/>Display and/or download QC data for project lanes</p>
-        
-            <p><a href="sample-mapping/sample_mapping.pl">Sample ID Mapping</a><br/>Download and/or view mappings of sample/supplier names and accession numbers</p>
-        
+            <p><a href="qc_grind/databases_view.pl">QC Grind</a><br/>Display and/or download QC data for project lanes</p> 
+    
+            <p><a href="hipsci_viewer/cohorts_view.pl">HipSci analyses</a><br/>Display analysis information for HipSci project cell lines</p>
+            
+            <p><a href="sample_mapping/sample_mapping.pl">Sample ID Mapping</a><br/>Download and/or view mappings of sample/supplier names and accession numbers</p>
+           
             <p><a href="map-view/map_view.pl">Map View</a><br/>Display QC information for short-read sequencing to allow lanes to be passed/failed</p>
-        
-            <p><a href="pending-view/pending_view.pl">Pending View</a><br/>Display pending/started requests for libraries and sequences</p>
             
             <p><a href="disk_usage/vrpipe_disk_usage.pl">Vrpipe Disk Usage</a><br/>Display vrpipe disk usage for directories and pipelines</p>
+            
+            <p><a href="pending-view/pending_view.pl">Pending View</a><br/>Display pending/started requests for libraries and sequences</p>
            
         </fieldset>
         </div>];
