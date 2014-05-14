306bff614821bd14bd2b382e5fe49ad7eabb0659
Sendu Bala <sb10@sanger.ac.uk>
2014-05-14 16:05:18 +0100

    no longer update uk10k farm, which no longer exists

--- scripts/update_softwarevertres.pl
+++ scripts/update_softwarevertres.pl
@@ -7,18 +7,18 @@ my $base = '/software/vertres';
 
 # update the checkouts that PATH and PERL5LIB point to
 foreach my $repo ("$base/bin-external", "$base/codebase", "$base/vrpipe/master", "$base/update_pipeline", "$base/conf", "$base/update_pipeline_hipsci") {
-    foreach my $server ('uk10k-login', 'vr-login') {
+    foreach my $server ('vr-login') {
         warn "\nupdating $repo on $server\n";
         system(qq[ssh -A $server "umask 002; cd $repo; git checkout .; git pull"]);
     }
 }
 
 # rsync things from precise /software/vertres to lenny-only /software/vertres
-my @to_rsync = qw(/software/vertres/etc/ /software/vertres/vrpipe/archive_disc_pool /software/vertres/vrpipe/scratch107_disc_pool /software/vertres/vrpipe/siteconfig/);
-foreach my $path (@to_rsync) {
-    warn "\nrsyncing $path\n";
-    system("rsync -rlptOD -z --delete $path uk10k-login:$path");
-}
+#my @to_rsync = qw(/software/vertres/etc/ /software/vertres/vrpipe/archive_disc_pool /software/vertres/vrpipe/scratch107_disc_pool /software/vertres/vrpipe/siteconfig/);
+#foreach my $path (@to_rsync) {
+#    warn "\nrsyncing $path\n";
+#    system("rsync -rlptOD -z --delete $path uk10k-login:$path");
+#}
 
 exit;
 
