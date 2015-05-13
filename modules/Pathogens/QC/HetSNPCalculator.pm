9110c1bd7385418ab751078c6dc98f3adaf08af8
Jorge Soares <js21@sanger.ac.uk>
2015-05-13 18:23:42 +0100

    All commands are now being built. Will have to change a couple of them to make it simpler and cleaner

--- modules/Pathogens/QC/HetSNPCalculator.pm
+++ modules/Pathogens/QC/HetSNPCalculator.pm
@@ -1,7 +1,7 @@
 package Pathogens::QC::HetSNPCalculator;
 
 use Moose;
-
+use File::Spec;
 
 #executables
 has 'samtools' => ( is => 'ro', isa => 'Str', required => 1 );
@@ -15,109 +15,110 @@ has 'bcft_min_qual' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'bcft_dp4_ref_allele_ratio' => ( is => 'ro', isa => 'Str', required => 1 );
 
 #Misc info
+has 'fa_ref' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'reference_size' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'lane_path' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'lane' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'sample_dir' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'heterozygosity_report_file_name' => ( is => 'ro', isa => 'Str', required => 1 );
 
-has 'full_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_full_path' );
-has 'bam_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_bam_file_path' );
-has 'temp_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_temp_vcf_file_path' );
-has 'snp_called_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_snp_called_vcf_file_path' );
-has 'filtered_snp_called_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_filtered_snp_called_file_path' );
-
-
+has 'full_path' => ( is => 'rw', isa => 'Str', lazy => 1, builder => 'build_full_path' );
+has 'mpileup_command' => ( is => 'rw', isa => 'Str', lazy => 1, builder => 'build_mpileup_command' );
+has 'total_number_of_snps_command' => ( is => 'rw', isa => 'Str', lazy => 1, builder => 'build_total_number_of_snps_command' );
+has 'snp_call_command' => ( is => 'rw', isa => 'Str', lazy => 1, builder => 'build_snp_call_command' );
+has 'bcf_query_filter_command' => ( is => 'rw', isa => 'Str', lazy => 1, builder => 'build_bcf_query_filter_command' );
 
 sub build_full_path {
 
   my ($self) = @_;
-  my $full_path = $self->lane_path . q(/) . $self->sample_dir . q(/);
+  my $full_path = File::Spec->catfile($self->lane_path, $self->sample_dir);
   return($full_path);
 }
 
-sub build_temp_vcf_file_path {
+sub _file_path {
 
-  my ($self) = @_;
-  my $path = $self->full_path . $self->{lane} . q(_temp_vcf.vcf.gz);
-  return($path);
-
-}
-
-sub build_snp_called_vcf_file_path {
-
-  my ($self) = @_;
-  my $path = $self->full_path . $self->lane . q(_snp_called.vcf.gz);
-  return($path);
-}
-
-sub build_filtered_snp_called_file_path {
-
-  my ($self) = @_;
-  my $path = $self->full_path . $self->lane . q(_filtered_snp_called.vcf);
-  return($path)
-
-}
-
-sub build_bam_file_path {
-
-  my ($self) = @_;
-  my $path = $self->full_path . $self->lane . q(.bam);
+  my ($self,$suffix) = @_;
+  my $path = File::Spec->catfile($self->full_path, $self->{lane} . $suffix);
   return($path);
 }
 
 sub build_mpileup_command {
 
+  my ($self) = @_;
 
+  my $bam_file = _file_path( $self, q(.bam) );
+  my $temp_vcf = _file_path( $self, q(_temp_vcf.vcf.gz) );
+  my $cmd = $self->samtools . q( mpileup -d 500 -t INFO/DPR,DV -C50 -ugf );
+  $cmd .= $self->fa_ref . q( ) . $bam_file . q( | bgzip > ) . $temp_vcf;
+
+  return($cmd);
 }
 
 
 sub build_total_number_of_snps_command {
 
+  my ($self) = @_;
 
+  my $temp_vcf = _file_path( $self, q(_temp_vcf.vcf.gz) );
+  my $cmd = $self->{bcftools} . q( call -m -f GQ,GP ) . $temp_vcf . q( | egrep -v "^#|DP=0" | wc -l);
+
+  return($cmd);
 }
 
 
 sub build_snp_call_command {
 
+  my ($self) = @_;
 
+  my $temp_vcf = _file_path( $self, q(_temp_vcf.vcf.gz) );
+  my $snp_called_vcf = _file_path( $self, q(_snp_called.vcf.gz) );
+  my $cmd = $self->bcftools . q( call -vm -O z ) . $temp_vcf . q( > ) . $snp_called_vcf;
 
+  return($cmd);
 }
 
 
 sub build_bcf_query_filter_command {
 
+  my ($self) = @_;
 
+  my $snp_called_vcf = _file_path( $self, q(_snp_called.vcf.gz) );
+  my $filtered_snp_called_vcf = _file_path( $self, q(_filtered_snp_called.vcf) );
+  my $cmd = $self->{bcftools} . q( filter -i );
+  $cmd .= q{"(DP4[0]+DP4[1])/(DP4[2]+DP4[3]) > 0.3" } . $snp_called_vcf . q{ | };
+  $cmd .= $self->{bcftools} . q( filter -i);
+  $cmd .= q{ "MIN(DP) >= } . $self->{bcft_min_dp};
+  $cmd .= q{ & MIN(DV) >= } . $self->{bcft_min_dv};
+  $cmd .= q{ & MIN(DV/DP)>= } . $self->{bcft_dp_dv_ratio};
+  $cmd .= q{ & QUAL >= } . $self->{bcft_min_qual};
+  $cmd .= q{ & (GT='1/0' | GT='0/1' | GT='1/2')" -};
+  $cmd .= q{ > } . $filtered_snp_called_vcf;
+
+  return($cmd);
 }
-
-
-sub build_vcf_header_command {
-
-
-}
-
 
 sub get_total_number_of_snps {
 
+  my ($self) = @_;
 
 }
 
 sub get_number_of_het_snps {
 
   my ($self) = @_;
-  my $temp_vcf_file_path = $self->temp_vcf_file_path;
-  my $snp_called_vcf_file_path = $self->snp_called_vcf_file_path;
-  my $filtered_snp_called_vcf_file_path = $self->temp_vcf_file_path;
+
 
 }
 
 sub calculate_percentage {
 
+  my ($self) = @_;
 
 }
 
 sub write_het_report {
 
+  my ($self) = @_;
 
 }
 
--- t/Pathogens/QC/HetSNPCalculator.t
+++ t/Pathogens/QC/HetSNPCalculator.t
@@ -10,6 +10,7 @@ BEGIN {
 
 my $samtools_exe = 'samtools-1.1.30';
 my $bcftools_exe = 'bcftools-1.2';
+my $fa_ref = 'path/to_refs/Neisseria_meningitidis_serogroup_C_FAM18_v1.fa';
 my $reference_size = 2194961;
 my $lane_path = '/lane/path';
 my $lane = '1234_4#2';
@@ -24,6 +25,7 @@ my $bcft_dp4_ref_allele_ratio = 0.3;
 ok my $hsc = Pathogens::QC::HetSNPCalculator->new(
 						  samtools => $samtools_exe,
 						  bcftools => $bcftools_exe,
+						  fa_ref => $fa_ref,
 						  reference_size => $reference_size,
 						  lane_path => $lane_path,
 						  lane => $lane,
@@ -38,6 +40,7 @@ ok my $hsc = Pathogens::QC::HetSNPCalculator->new(
 
 is ( $hsc->samtools, $samtools_exe, 'Samtools exe' );
 is ( $hsc->bcftools, $bcftools_exe, 'Bcftools exe' );
+is ( $hsc->fa_ref, $fa_ref, 'Fasta ref' );
 is ( $hsc->reference_size, $reference_size, 'Reference size' );
 is ( $hsc->lane_path, $lane_path, 'Lane path' );
 is ( $hsc->lane, $lane, 'Lane' );
@@ -49,12 +52,11 @@ is ( $hsc->bcft_dp_dv_ratio, $bcft_dp_dv_ratio, 'DP/DV threshold' );
 is ( $hsc->bcft_min_qual, $bcft_min_qual, 'Min QUAL threshold' );
 is ( $hsc->bcft_dp4_ref_allele_ratio, $bcft_dp4_ref_allele_ratio, 'DP4 Ref/Alt threshold' );
 
-
-is ( $hsc->bam_file_path, '/lane/path/qc-sample/1234_4#2.bam', 'Bam file path');
-is ( $hsc->temp_vcf_file_path, '/lane/path/qc-sample/1234_4#2_temp_vcf.vcf.gz', 'Temp vcf file path');
-is ( $hsc->snp_called_vcf_file_path, '/lane/path/qc-sample/1234_4#2_snp_called.vcf.gz',  'SNP called vcf file path');
-is ( $hsc->filtered_snp_called_vcf_file_path, '/lane/path/qc-sample/1234_4#2_filtered_snp_called.vcf', 'Filtered SNP called vcf file path');
-
+is ( $hsc->full_path, '/lane/path/qc-sample', 'Full path');
+is ( $hsc->mpileup_command, q(samtools-1.1.30 mpileup -d 500 -t INFO/DPR,DV -C50 -ugf path/to_refs/Neisseria_meningitidis_serogroup_C_FAM18_v1.fa /lane/path/qc-sample/1234_4#2.bam | bgzip > /lane/path/qc-sample/1234_4#2_temp_vcf.vcf.gz), 'mpileup command');
+is ( $hsc->total_number_of_snps_command, q(bcftools-1.2 call -m -f GQ,GP /lane/path/qc-sample/1234_4#2_temp_vcf.vcf.gz | egrep -v "^#|DP=0" | wc -l), 'total number of snps command');
+is ( $hsc->snp_call_command, q(bcftools-1.2 call -vm -O z /lane/path/qc-sample/1234_4#2_temp_vcf.vcf.gz > /lane/path/qc-sample/1234_4#2_snp_called.vcf.gz), 'snp call command');
+is ( $hsc->bcf_query_filter_command, q(bcftools-1.2 filter -i "(DP4[0]+DP4[1])/(DP4[2]+DP4[3]) > 0.3" /lane/path/qc-sample/1234_4#2_snp_called.vcf.gz | bcftools-1.2 filter -i "MIN(DP) >= 10 & MIN(DV) >= 5 & MIN(DV/DP)>= 0.3 & QUAL >= 20 & (GT='1/0' | GT='0/1' | GT='1/2')" - > /lane/path/qc-sample/1234_4#2_filtered_snp_called.vcf), 'bcf query filter command');
 print Dumper($hsc);
 
 done_testing();
