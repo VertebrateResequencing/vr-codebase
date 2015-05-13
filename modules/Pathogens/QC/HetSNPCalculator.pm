6fa7bcf16cdf3e5a06d32c4e49d89d43dbe430b5
Jorge Soares <js21@sanger.ac.uk>
2015-05-13 17:02:50 +0100

    All required attributes now in place and tested

--- modules/Pathogens/QC/HetSNPCalculator.pm
+++ modules/Pathogens/QC/HetSNPCalculator.pm
@@ -2,19 +2,32 @@ package Pathogens::QC::HetSNPCalculator;
 
 use Moose;
 
+
+#executables
+has 'samtools' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'bcftools' => ( is => 'ro', isa => 'Str', required => 1 );
+
+#bcftools filters
+has 'bcft_min_dp' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'bcft_min_dv' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'bcft_dp_dv_ratio' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'bcft_min_qual' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'bcft_dp4_ref_allele_ratio' => ( is => 'ro', isa => 'Str', required => 1 );
+
+#Misc info
 has 'reference_size' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'lane_path' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'lane' => ( is => 'ro', isa => 'Str', required => 1 );
 has 'sample_dir' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'heterozygosity_report_file_name' => ( is => 'ro', isa => 'Str', required => 1 );
+
 has 'full_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_full_path' );
+has 'bam_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_bam_file_path' );
 has 'temp_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_temp_vcf_file_path' );
 has 'snp_called_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_snp_called_vcf_file_path' );
+has 'filtered_snp_called_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_filtered_snp_called_file_path' );
 
-#my $lane_path = $self->{lane_path};
-#my $full_path = $self->{lane_path} . q(/) . $self->{sample_dir} . q(/);
-#my $temp_vcf_file = $full_path . $self->{lane} . q(_temp_vcf.vcf.gz);
-#my $snp_called_vcf_file = $full_path . $self->{lane} . q(_snp_called.vcf.gz);
-#my $filtered_snp_called_vcf_file = $full_path . $self->{lane} . q(_filtered_snp_called.vcf);
+
 
 sub build_full_path {
 
@@ -26,24 +39,31 @@ sub build_full_path {
 sub build_temp_vcf_file_path {
 
   my ($self) = @_;
-  my $temp_vcf_file = $self->full_path . $self->{lane} . q(_temp_vcf.vcf.gz);
-  return($temp_vcf_file);
+  my $path = $self->full_path . $self->{lane} . q(_temp_vcf.vcf.gz);
+  return($path);
 
 }
 
 sub build_snp_called_vcf_file_path {
 
   my ($self) = @_;
-  my $snp_called_vcf_file = $self->full_path . $self->lane . q(_snp_called.vcf.gz);
-  return($snp_called_vcf_file);
+  my $path = $self->full_path . $self->lane . q(_snp_called.vcf.gz);
+  return($path);
 }
 
 sub build_filtered_snp_called_file_path {
 
   my ($self) = @_;
-  my $filtered_snp_called_vcf_file_path = $self->full_path . $self->lane . q(_filtered_snp_called.vcf);
-  return($filtered_snp_called_vcf_file_path)
+  my $path = $self->full_path . $self->lane . q(_filtered_snp_called.vcf);
+  return($path)
 
+}
+
+sub build_bam_file_path {
+
+  my ($self) = @_;
+  my $path = $self->full_path . $self->lane . q(.bam);
+  return($path);
 }
 
 sub build_mpileup_command {
@@ -79,11 +99,15 @@ sub build_vcf_header_command {
 
 sub get_total_number_of_snps {
 
-}
 
+}
 
 sub get_number_of_het_snps {
 
+  my ($self) = @_;
+  my $temp_vcf_file_path = $self->temp_vcf_file_path;
+  my $snp_called_vcf_file_path = $self->snp_called_vcf_file_path;
+  my $filtered_snp_called_vcf_file_path = $self->temp_vcf_file_path;
 
 }
 
--- t/Pathogens/QC/HetSNPCalculator.t
+++ t/Pathogens/QC/HetSNPCalculator.t
@@ -8,17 +8,52 @@ BEGIN {
     use_ok('Pathogens::QC::HetSNPCalculator');
 }
 
+my $samtools_exe = 'samtools-1.1.30';
+my $bcftools_exe = 'bcftools-1.2';
 my $reference_size = 2194961;
-my $lane_path = '/lane/path/';
+my $lane_path = '/lane/path';
 my $lane = '1234_4#2';
 my $sample_dir = 'qc-sample';
-ok my $hsc = Pathogens::QC::HetSNPCalculator->new( reference_size => $reference_size, lane_path => $lane_path, lane => $lane, sample_dir => $sample_dir ), 'create instance of object';
+my $het_report = 'heterozygosity_report.txt';
+my $bcft_min_dp = 10;
+my $bcft_min_dv = 5;
+my $bcft_dp_dv_ratio = 0.3;
+my $bcft_min_qual = 20;
+my $bcft_dp4_ref_allele_ratio = 0.3;
 
+ok my $hsc = Pathogens::QC::HetSNPCalculator->new(
+						  samtools => $samtools_exe,
+						  bcftools => $bcftools_exe,
+						  reference_size => $reference_size,
+						  lane_path => $lane_path,
+						  lane => $lane,
+						  sample_dir => $sample_dir,
+						  heterozygosity_report_file_name => $het_report,
+						  bcft_min_dp => 10,
+						  bcft_min_dv => 5,
+						  bcft_dp_dv_ratio => 0.3,
+						  bcft_min_qual => 20,
+						  bcft_dp4_ref_allele_ratio => 0.3,
+						 ), 'create instance of object';
+
+is ( $hsc->samtools, $samtools_exe, 'Samtools exe' );
+is ( $hsc->bcftools, $bcftools_exe, 'Bcftools exe' );
 is ( $hsc->reference_size, $reference_size, 'Reference size' );
 is ( $hsc->lane_path, $lane_path, 'Lane path' );
 is ( $hsc->lane, $lane, 'Lane' );
 is ( $hsc->sample_dir, $sample_dir, 'Sample dir' );
+is ( $hsc->heterozygosity_report_file_name, $het_report, 'Het report file name' );
+is ( $hsc->bcft_min_dp, $bcft_min_dp, 'DP threshold' );
+is ( $hsc->bcft_min_dv, $bcft_min_dv, 'DV threshold' );
+is ( $hsc->bcft_dp_dv_ratio, $bcft_dp_dv_ratio, 'DP/DV threshold' );
+is ( $hsc->bcft_min_qual, $bcft_min_qual, 'Min QUAL threshold' );
+is ( $hsc->bcft_dp4_ref_allele_ratio, $bcft_dp4_ref_allele_ratio, 'DP4 Ref/Alt threshold' );
 
+
+is ( $hsc->bam_file_path, '/lane/path/qc-sample/1234_4#2.bam', 'Bam file path');
+is ( $hsc->temp_vcf_file_path, '/lane/path/qc-sample/1234_4#2_temp_vcf.vcf.gz', 'Temp vcf file path');
+is ( $hsc->snp_called_vcf_file_path, '/lane/path/qc-sample/1234_4#2_snp_called.vcf.gz',  'SNP called vcf file path');
+is ( $hsc->filtered_snp_called_vcf_file_path, '/lane/path/qc-sample/1234_4#2_filtered_snp_called.vcf', 'Filtered SNP called vcf file path');
 
 print Dumper($hsc);
 
