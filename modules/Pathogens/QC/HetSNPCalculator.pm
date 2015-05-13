809c56b6f3b6753b7d0589bc20526c04188565b5
Jorge Soares <js21@sanger.ac.uk>
2015-05-13 16:28:30 +0100

    Required attributes in place and tested

--- modules/Pathogens/QC/HetSNPCalculator.pm
+++ modules/Pathogens/QC/HetSNPCalculator.pm
@@ -3,6 +3,12 @@ package Pathogens::QC::HetSNPCalculator;
 use Moose;
 
 has 'reference_size' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'lane_path' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'lane' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'sample_dir' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'full_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_full_path' );
+has 'temp_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_temp_vcf_file_path' );
+has 'snp_called_vcf_file_path' => ( is => 'ro', isa => 'Str', lazy => 1, builder => 'build_snp_called_vcf_file_path' );
 
 #my $lane_path = $self->{lane_path};
 #my $full_path = $self->{lane_path} . q(/) . $self->{sample_dir} . q(/);
@@ -10,10 +16,33 @@ has 'reference_size' => ( is => 'ro', isa => 'Str', required => 1 );
 #my $snp_called_vcf_file = $full_path . $self->{lane} . q(_snp_called.vcf.gz);
 #my $filtered_snp_called_vcf_file = $full_path . $self->{lane} . q(_filtered_snp_called.vcf);
 
+sub build_full_path {
 
+  my ($self) = @_;
+  my $full_path = $self->lane_path . q(/) . $self->sample_dir . q(/);
+  return($full_path);
+}
 
-sub build_filenames {
+sub build_temp_vcf_file_path {
 
+  my ($self) = @_;
+  my $temp_vcf_file = $self->full_path . $self->{lane} . q(_temp_vcf.vcf.gz);
+  return($temp_vcf_file);
+
+}
+
+sub build_snp_called_vcf_file_path {
+
+  my ($self) = @_;
+  my $snp_called_vcf_file = $self->full_path . $self->lane . q(_snp_called.vcf.gz);
+  return($snp_called_vcf_file);
+}
+
+sub build_filtered_snp_called_file_path {
+
+  my ($self) = @_;
+  my $filtered_snp_called_vcf_file_path = $self->full_path . $self->lane . q(_filtered_snp_called.vcf);
+  return($filtered_snp_called_vcf_file_path)
 
 }
 
--- t/Pathogens/QC/HetSNPCalculator.t
+++ t/Pathogens/QC/HetSNPCalculator.t
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Data::Dumper;
 
 BEGIN {
     use Test::Most ;
@@ -8,7 +9,17 @@ BEGIN {
 }
 
 my $reference_size = 2194961;
-ok my $hsc = Pathogens::QC::HetSNPCalculator->new(reference_size => $reference_size), 'create instance of object';
+my $lane_path = '/lane/path/';
+my $lane = '1234_4#2';
+my $sample_dir = 'qc-sample';
+ok my $hsc = Pathogens::QC::HetSNPCalculator->new( reference_size => $reference_size, lane_path => $lane_path, lane => $lane, sample_dir => $sample_dir ), 'create instance of object';
 
+is ( $hsc->reference_size, $reference_size, 'Reference size' );
+is ( $hsc->lane_path, $lane_path, 'Lane path' );
+is ( $hsc->lane, $lane, 'Lane' );
+is ( $hsc->sample_dir, $sample_dir, 'Sample dir' );
+
+
+print Dumper($hsc);
 
 done_testing();
