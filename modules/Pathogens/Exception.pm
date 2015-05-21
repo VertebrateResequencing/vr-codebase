e60872b78cbeab1d96f05622622a2b3df17447ab
Jorge Soares <js21@sanger.ac.uk>
2015-05-21 09:57:30 +0100

    Simply ran perltiddy on the previous commit

--- modules/Pathogens/Exception.pm
+++ modules/Pathogens/Exception.pm
@@ -1,9 +1,10 @@
 package Pathogens::Exception;
 
 use Exception::Class (
-		      Pathogens::Exception::NullDenominator => { description => 'Denominator is 0' },
-		      Pathogens::Exception::HetSNPStepCommand => { description => 'Problems running the shell command' },
-		     );
-
+    Pathogens::Exception::NullDenominator =>
+      { description => 'Denominator is 0' },
+    Pathogens::Exception::HetSNPStepCommand =>
+      { description => 'Problems running the shell command' },
+);
 
 1;
