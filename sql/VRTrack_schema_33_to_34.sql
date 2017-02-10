e263e485915aab8adf05ef28a931f903ba9758fb
andrewjpage <andrewjpage@gmail.com>
2017-02-10 16:01:07 +0000

    extra index

--- sql/VRTrack_schema_33_to_34.sql
+++ sql/VRTrack_schema_33_to_34.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `individual` ADD INDEX `acc` (`acc`);
+ALTER TABLE library ADD INDEX library_id_sample_id_latest (library_id,sample_id,latest);
+ALTER TABLE lane ADD INDEX library_id_latest (library_id,latest);
 update schema_version set schema_version=34;
