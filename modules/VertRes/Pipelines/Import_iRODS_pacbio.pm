4e9dfb635439d7db31f196425252b480985cfc12
andrewjpage <andrewjpage@gmail.com>
2017-11-07 11:26:02 +0000

    three threads for import

--- modules/VertRes/Pipelines/Import_iRODS_pacbio.pm
+++ modules/VertRes/Pipelines/Import_iRODS_pacbio.pm
@@ -168,7 +168,7 @@ sub _bas_h5_filenames {
 sub convert_to_fastq {
     my ( $self, $lane_path, $lock_file ) = @_;
     my $memory_in_mb = 2500;
-	my $threads = 2;
+	my $threads = 3;
 
     my $prefix   = $$self{prefix};
     my $work_dir = $lane_path;
